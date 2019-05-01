24b9fd0389d9fcdd40a8320436bc80eb42db5ebe
roberto0412 <roberto_esquivel04@outlook.com>
2019-04-30 17:54:38 -0700

    Finalized Project

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-const s = 'hellow world';
-console.log(s.split(','));
+document.addEventListener("DOMContentLoaded",function() {
+
+    const navLinks = document.querySelectorAll('.navegation ul li a');
+    navLinks.forEach(function(element){
+        element.addEventListener('click', function(event){
+            event.preventDefault();
+            console.log('event', event);
+
+        
+
+            document.querySelector(event.target.hash).scrollIntoView({
+                behavior:'smooth'
+            });
+    });
+
+    });
+})
+
+
+const form = document.querySelector('.sign-up');
+form.addEventListener('button',function(event){
+    event.preventDefault();
+    alert('Thanks for subscribing!');
+})
