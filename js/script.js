0c207ac01030628a71076c2189a29af3c89a8437
roberto <roberto_esquivel04@outlook.com>
2019-08-25 15:50:42 -0700

    added smooth scroll js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".navegation ul li a");
+    navLinks.forEach(function(element) {
+      element.addEventListener("click", function(event) {
+        event.preventDefault();
+        console.log("event", event);
 
-    const navLinks = document.querySelectorAll('.navegation ul li a');
-    navLinks.forEach(function(element){
-        element.addEventListener('click', function(event){
-            event.preventDefault();
-            console.log('event', event);
-
-        
-
-            document.querySelector(event.target.hash).scrollIntoView({
-                behavior:'smooth'
-            });
+        document.querySelector(event.target.hash).scrollIntoView({
+          behavior: "smooth"
+        });
+      });
     });
-
-    });
-})
-
-
-const form = document.querySelector('.sign-up');
-form.addEventListener('button',function(event){
-    event.preventDefault();
-    alert('Thanks for subscribing!');
-})
+  });
